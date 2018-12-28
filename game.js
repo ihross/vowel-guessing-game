974536f989d8a29536b55038495450855ee3bd3b
Ian Ross <ianhowesross@gmail.com>
2018-12-28 14:18:30 -0500

    Setting up a rough timer

--- game.js
+++ game.js
@@ -3,6 +3,27 @@ window.addEventListener('load', function () {
         title.addEventListener("click", () => {
             console.log("Title clicked")
         });
+
+    const minutesLabel = document.getElementById("minutes");
+    const secondsLabel = document.getElementById("seconds");
+    let totalSeconds = 0;
+    setInterval(setTime, 1000);
+
+    function setTime() {
+        totalSeconds++;
+        secondsLabel.innerHTML = pad(totalSeconds % 60);
+        minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+    }
+
+    function pad(val) {
+        let valString = val + "";
+        if (valString.length < 2) {
+            return "0" + valString;
+        } else {
+            return valString;
+        }
+    }
+
 }, false);
 
 
