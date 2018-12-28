6b28776df351457dbbd226e65f983982e80fa7b6
Ian Ross <ianhowesross@gmail.com>
2018-12-28 13:51:08 -0500

    Added event listener when title is clicked

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-console.log("JavaScript activated");
+window.addEventListener('load', function () {
+    const title = document.getElementById("title");
+        title.addEventListener("click", () => {
+            console.log("Title clicked")
+        });
+}, false);
+
+
+
+// document.getElementsByClassName('title');
+// document.getElementsByClassName('content');
+
+
+
+// // title.addEventListener('click', function () {
+// //     console.log("You've clicked the title!");
+// // });
+
+
+// console.log("JavaScript activated");
+
