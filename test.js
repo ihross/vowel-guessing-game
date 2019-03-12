8ea8c25a1e4c468a102959795a9e6affc5762d00
Ian Ross <ianhowesross@gmail.com>
2019-03-12 15:10:14 -0400

    Extra tests removed

--- test.js
+++ test.js
@@ -20,16 +20,3 @@
 
 
 // }
-
-
-for (let i = 1; i <= 100; i++) {
-    if (i % 15 === 0) {
-        console.log("CracklePop");
-    } else if (i % 3 === 0) {
-        console.log("Crackle");
-    } else if (i % 5 === 0) {
-        console.log("Pop");
-    } else {
-        console.log(i);
-    }
-}
